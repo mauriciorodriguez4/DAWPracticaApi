ebeac35c3be540a70cf4adc8adc1c2f442af5215
rodma <rodma@MAURIROD>
2023-02-25 07:12:07 -0600

    Agregar archivos de proyecto.

---
+++ webApiPractica/Controllers/equiposController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webApiPractica.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace webApiPractica.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class equiposController : ControllerBase
+    {
+        private readonly  EquiposContext _equiposContext;
+        public equiposController(EquiposContext equiposContexto) 
+        {
+            _equiposContext = equiposContexto;
+        
+        }
+        /// <summary>
+        // EndPoint que retorna el listado de todos los equipos existentes
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult Get()
+        {
+            List<equipos> listadoequipo = (from e in _equiposContext.equipos select e).ToList();
+
+            if(listadoequipo.Count()==0)
+            {
+                return NotFound();
+            }
+                        
+            return Ok(listadoequipo);
+        }
+    }
+}
---
+++ webApiPractica/Models/EquiposContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace webApiPractica.Models
+{
+    public class EquiposContext : DbContext
+
+    {
+        public EquiposContext(DbContextOptions<EquiposContext>options) : base(options)
+        {
+
+        }
+
+        public DbSet<equipos> equipos { get; set; }
+    }
+}
